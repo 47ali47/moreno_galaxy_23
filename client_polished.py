bde598c3785aa84c449bc7f99b2a3a74140e34e3
47ali47 <anbo.li843@gmail.com>
2023-08-13 13:47:38 -0400

    added the mass density plot

--- client_polished.py
+++ client_polished.py
@@ -21,30 +21,37 @@ stars_mass = data_obj.particle_data['stars_mass']
 def radius(x, y):
     return math.sqrt(x**2+y**2)
 
+
+def calculate_ring_area(inner_radius, outer_radius):
+    return math.pi * ( outer_radius**2 - inner_radius**2)
+
+
 def handle_ring(ring_criteria, all_stars):
     """
     Identify stars within the ring according to ring_criteria. 
-    Then return the number of stars in the ring and their total mass.
+    Then return the number of stars in the ring and their total mass and their mass density.
     :param ring_criteria: (inner_radius, outer_radius)
     :param all_stars: [((x, y), mass)...((x, y)), mass)]
-    :return: (number_of_stars_in_the_ring, total_star_mass_in_the_ring)
+    :return: (number_of_stars_in_the_ring, total_star_mass_in_the_ring, ring_mass_density)
     """
     (radius_inner, radius_outer) = ring_criteria
     star_count = 0
     total_star_mass = 0
+    ring_area = calculate_ring_area(radius_inner, radius_outer) 
+    #identify stars in the ring, star count, and their total mass
     for star in all_stars:
         #unpacking the variables
         ((x,y), mass) = star
         if radius_inner < radius(x,y) <= radius_outer: #inside the ring
             star_count = star_count + 1
             total_star_mass = total_star_mass + mass
-    return (star_count, total_star_mass)
+    return (star_count, total_star_mass, total_star_mass/ring_area)
 
 #all_stars is a list of the stars coordinate and mass
 #[((x, y), mass)...((x, y)), mass)]
 all_stars = []
 
-rings = {} #{ring_index:(star_count, total_star_mass_in_ring)}
+rings = {} #{ring_index:(star_count, total_star_mass_in_ring, ring_density)}
 
 #length is 36043 the number of the stars
 for i in range(len(stars_x)):
@@ -54,7 +61,7 @@ for i in range(len(stars_x)):
 
 #max radius = 17.96
 max_radius = data_obj.particle_data['object_Rvir']
-slice_thickness = 0.5
+slice_thickness = 0.1
 number_slices = math.ceil(max_radius/slice_thickness)
 
 #defining the rings
@@ -64,17 +71,6 @@ for slice_index in range(number_slices):
     rings[slice_index] = handle_ring((radius_inner, radius_outer), all_stars)
     radius_inner = radius_outer
     radius_outer = radius_inner + slice_thickness
-print('rings', rings)
-
-
-
-#for (ring_index, (star_count, total_star_mass)) in rings.items():
-    #print('ring index', ring_index)
-    #print('star_count', star_count)
-    #print('total_star_mass', total_star_mass)
-
-
-#plot for rings and total_mass/star_count
 
 
 def plot_star_rings(filename, title, x_label, x_values, y_label, y_values, label,
@@ -94,9 +90,10 @@ def plot_star_rings(filename, title, x_label, x_values, y_label, y_values, label
     plt.savefig(filename, format="pdf")
     os.system("open {}".format(filename))
 
+#StarMass graph
 plot_label = 'Ring Thickness = {}, \n Max Radius = {} KPC'.format(slice_thickness, max_radius)
 x_values = rings.keys() #the ring indexes e.g. 0, 1, 2, 3...
-y_values = [mass for (count, mass) in rings.values()]
+y_values = [mass for (count, mass, density) in rings.values()]
 plot_star_rings(filename = 'star_mass_plot.pdf',
                 title = 'Scatter Plot of Star Radius (in rings) vs. Mass',
                 x_label = 'Rings',
@@ -105,8 +102,9 @@ plot_star_rings(filename = 'star_mass_plot.pdf',
                 y_values = y_values,
                 label = plot_label)
 
+#StarCount graph
 x_values = rings.keys() #the ring indexes e.g. 0, 1, 2, 3...
-y_values = [count for (count, mass) in rings.values()]
+y_values = [count for (count, mass, density) in rings.values()]
 plot_star_rings(filename = 'star_count_plot.pdf',
                 title = 'Scatter Plot of Star Radius (in rings) vs. Star Count',
                 x_label = 'Rings',
@@ -116,9 +114,8 @@ plot_star_rings(filename = 'star_count_plot.pdf',
                 label = plot_label)  
 
 #accumlated star mass graph plot
-
 ring_indexes = rings.keys() #the ring indexes e.g. 0, 1, 2, 3...
-ring_star_mass = [mass for (count, mass) in rings.values()]
+ring_star_mass = [mass for (count, mass, density) in rings.values()]
 accumlated_star_mass = []
 for i in range(len(ring_star_mass)):
     if i == 0:
@@ -126,9 +123,20 @@ for i in range(len(ring_star_mass)):
     else:
         accumlated_star_mass.append(ring_star_mass[i]+accumlated_star_mass[i-1])
 plot_star_rings(filename = 'accumulated_star_mass_plot.pdf',
-                title = 'Scatter Plot of Star Radius (in rings) vs. Accumulated Mass',
+                title = 'Scatter Plot of Star Radius (in rings) vs. Accumulated Star Mass',
                 x_label = 'Rings',
                 x_values = ring_indexes,
                 y_label = 'Accumulated Star Mass',
                 y_values = accumlated_star_mass,
                 label = plot_label)   
+#Star_density plot
+lot_label = 'Ring Thickness = {}, \n Max Radius = {} KPC'.format(slice_thickness, max_radius)
+x_values = rings.keys() #the ring indexes e.g. 0, 1, 2, 3...
+y_values = [density for (count, mass, density) in rings.values()]
+plot_star_rings(filename = 'star_mass_density_plot.pdf',
+                title = 'Scatter Plot of Star Radius (in rings) vs. Star Density',
+                x_label = 'Rings',
+                x_values = x_values,
+                y_label = 'Star Density',
+                y_values = y_values,
+                label = plot_label)
